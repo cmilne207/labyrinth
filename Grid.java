af1df571bd2bbd3d93a5be76fecf9a1a5a2cfe51
Joe-Pakulski <jpakulsk@uvm.edu>
2017-04-04 17:31:07 -0400

    Add files via upload
    Basic grid and Tile class.

---
+++ Grid.java
@@ -0,0 +1,17 @@
+//Rrpresentation of the board
+//7x7 grid treated as a two dimensional array
+//Joe Pakulski
+
+import java.util.Arrays; 
+import java.util.*;
+
+
+public class Grid 
+{     
+
+  public static void main( String[] args )
+  {
+    Tile[][] grid = new Tile[7][7];
+    System.out.println(Arrays.deepToString(grid));
+  }
+}
---
+++ Tile.java
@@ -0,0 +1,14 @@
+//Rrpresentation of a tile
+//Tile is a part of the maze
+//It has four different directions
+//Joe Pakulski
+
+import java.util.Arrays; 
+import java.util.*;
+
+
+public class Tile
+{     
+  
+
+}
