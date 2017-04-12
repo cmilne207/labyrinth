358aab72b08b0548b7b29f169eea1480ecee6e07
cmilne207 <cmilne207@gmail.com>
2017-04-11 23:19:46 -0400

    fixing stuff in Deck

--- Deck.java
+++ Deck.java
@@ -1,1 +1,46 @@
+import java.util.Random;
+import java.util.ArrayList;
+  
+//Deck of path tiles used for populating the board
 
+public class Deck{	
+	//Variables
+	final int CARDS_IN_DECK = 34;
+	ArrayList<Tile> deck;
+	
+	//constructor
+	//creates empty deck, populates it, then shuffles it
+	public Deck(){
+		deck = new ArrayList<Tile>();
+
+		////////////////////////////////////////////////////////////
+		//add code to create all cards and populate the deck
+		////////////////////////////////////////////////////////////
+ 
+ 		shuffle();
+	}
+
+	//returns card on the top of the deck
+	public Tile draw(){
+		Tile t = deck.remove(0);
+		return t;
+	}
+  
+  	//returns the number of cards still in the deck
+  	public int cardsRemaining(){  
+  		return deck.size();
+  	}
+    
+  	//shuffle alg
+  	public void shuffle(){
+  		int randNum;
+  		Tile temp;
+  		Random r = new Random();
+  		for (int i = 0; i < deck.size(); i++){
+  			randNum = r.nextInt(deck.size());
+  			temp = deck.get(i);
+  			deck.set(i,deck.get(randNum));
+  			deck.set(randNum,temp);
+   		}
+   	}
+}
