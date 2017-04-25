81167ac418e4f00c39ff2e32e1316a994699dfca
cmilne207 <cmilne207@gmail.com>
2017-04-25 17:28:40 -0400

    player and card

--- Card.java
+++ Card.java
@@ -1,1 +1,16 @@
+//Deck of path tiles used for populating the board
 
+public class Card{	
+	//variables
+	int treasureIndex;
+	
+	//constructors
+	public Card(int newIndex){
+		treasureIndex = newIndex;
+	}
+	
+	//getters
+	public int getTreasureIndex(){
+		return treasureIndex;
+	}
+}
--- Player.java
+++ Player.java
@@ -1,1 +1,37 @@
+import java.util.ArrayList;
 
+//holds the game parts that correspond to one player or another baring location on the grid
+
+public class Player{	
+	//variables
+	private ArrayList<Card> treasures;
+	private Card curTreasure;
+	private int numberDrawn = 0;
+	private boolean won = false; 
+	
+	//constructor
+	public Player(ArrayList<Card> newTreasures){
+		treasures = newTreasures;
+		pullNextTreasure();
+	}
+	
+	//getters
+	public int getCurTreasure(){
+		return curTreasure.getTreasureIndex();
+	}
+	
+	public boolean checkForWin(){
+		return won;
+	}
+	
+	//setters
+	public void pullNextTreasure(){
+		if (numberDrawn < 12){
+			curTreasure = treasures.remove(numberDrawn);
+			numberDrawn++;
+		}
+		else{
+			won = true;
+		}
+	}
+}
