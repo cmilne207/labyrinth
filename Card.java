78c22522691adebc69a0ea5813b1ef4b3078d17d
Joe-Pakulski <jpakulsk@uvm.edu>
2017-04-27 17:30:31 -0400

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,16 +1,23 @@
 //Deck of path tiles used for populating the board
+//Created by Ben ammended by Joe
 
 public class Card{	
 	//variables
 	int treasureIndex;
+   String image;
 	
 	//constructors
-	public Card(int newIndex){
+	public Card(int newIndex,String newimage){
 		treasureIndex = newIndex;
+      image = newimage;
 	}
 	
 	//getters
 	public int getTreasureIndex(){
 		return treasureIndex;
+	}
+   
+   public String getImage(){
+		return image;
 	}
 }
